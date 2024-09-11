0e519dbc5d854ccba6dfcd0099e16a76695f4aa7
Lucas Azevedo <lucas.azevedodias@outlook.com>
2024-09-11 20:15:24 -0300

    a

---
+++ Python/Programação Distribuída/06/WS-Rest/rest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+
+class Eco:
+
+    def __init__(self, sufixo):
+        self.sufixo = sufixo
+        self.count = 0
+    
+    def diga(self, msg):
+        self.count += 1
+        return msg + self.sufixo
+
+
+class EcoApi(Resource):
+
+    def get(self):
+        return {"cont": eco.count}
+    
+    def patch(self, msg):
+        return {"resp": eco.diga(msg)}
+
+
+if __name__ == "__main__":
+    app = Flask(__name__)
+    api = Api(app)
+
+    eco = Eco("_ECO")
+    api.add_resource(EcoApi, "/eco", "/eco/<string:msg>")
+    app.run(port=1879, debug=True)
